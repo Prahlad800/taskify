edc3ad601aa1aeec83209f721ae8fdfb4f49ea21
prahlad800 <prahlad8009872468@gmail.com>
2026-04-12 04:06:16 +0530

    work task page

--- backend/router/task_router.js
+++ backend/router/task_router.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { auth_notes_page } from "../middleware/auth_notes.js";
-import { create_task_title,show_task_title,update_task_content,delete_task_note } from "../controllers/task_control.js";
+import { create_task_title,show_task_title,update_task_content,delete_task,show_task ,update_task_item} from "../controllers/task_control.js";
 
 const router =Router()
 
  router.post("/",auth_notes_page,create_task_title)
  router.get("/",auth_notes_page,show_task_title)
  router.put("/:id",auth_notes_page,update_task_content)
- router.delete("/:id",auth_notes_page,delete_task_note)
+ router.get("/:id",auth_notes_page,show_task)
+ router.delete("/:id",auth_notes_page,delete_task)
+ router.put("/:id/:todoId",auth_notes_page, update_task_item);
 //   router.get("/",auth_notes_page,show_title)
  
  
--- frontend/src/page/Task.jsx
+++ frontend/src/page/Task.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import Navbar_home from "../components/home/Navbar_home";
 import Bottome_home from "../components/home/Bottome_home";
 import "../components/home/home.css";
@@ -14,77 +13,144 @@ function Task() {
   const [taskLoading, setTaskLoading] = useState(false);
   const [showPopupTask, setShowPopupTask] = useState(false);
   const [newTitleTask, setNewTitleTask] = useState("");
-  const [priority, setPriority] = useState("");
-  const [taskDetails, setTaskDetails] = useState(null);
-  
+  const [priority, setPriority] = useState("medium");
+  const [showPopupDeleteTask, setShowPopupDeleteTask] = useState(false);
+
+  const [newTodoText, setNewTodoText] = useState("");
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [todosAddTask, setTodosAddTask] = useState([]);
 
   const [activeTab, setActiveTab] = useState("tasks");
   const [task, setTask] = useState([]);
-  const getToken = () => localStorage.getItem("token");
 
-  // create classname in title list
+  const getToken = () => localStorage.getItem("token");
 
   const baseURL =
     window.location.hostname === "localhost"
       ? "http://localhost:3030"
       : "https://taskify-gcxc.onrender.com";
-  // FETCH NOTES
+
+  // 🔥 FETCH ALL TASK
   const fetchNotes = async () => {
     setTaskLoading(true);
     try {
-      // setNotesLoading(true)
       const res = await axios.get(`${baseURL}/task`, {
         headers: { Authorization: `Bearer ${getToken()}` },
       });
-
       setTask(res.data.data);
-
-      console.log(res.data.data);
     } catch (error) {
       handleError("Failed to fetch notes", error);
     } finally {
-      setTaskLoading(false); // 🔥 ye ALWAYS chalega
+      setTaskLoading(false);
     }
-    //   console.log("loading:", notesLoading);
-    // console.log("notes:", notes);
   };
 
-  // add title
+  // 🔥 ADD TASK
   const handleSaveTask = async () => {
     if (!newTitleTask.trim()) return handleError("Title required");
 
     try {
       const res = await axios.post(
         `${baseURL}/task`,
-        {
-          title: newTitleTask,
-          priority: priority,
-        },
-        {
-          headers: { Authorization: `Bearer ${getToken()}` },
-        },
+        { title: newTitleTask, priority },
+        { headers: { Authorization: `Bearer ${getToken()}` } },
       );
 
       setTask((prev) => [...prev, res.data.data]);
       setShowPopupTask(false);
       setNewTitleTask("");
-
-      setPriority("medium"); // reset
+      setPriority("medium");
 
       handleSuccess("Task added");
     } catch (error) {
       handleError("Error adding task", error);
     }
   };
-  const handleTaskClick = async (id)=>{
-    
-  }
+
+  // 🔥 FETCH SINGLE TASK
+  const handleTaskClick = async (id) => {
+    try {
+      const res = await axios.get(`${baseURL}/task/${id}`, {
+        headers: { Authorization: `Bearer ${getToken()}` },
+      });
+
+      const todo = res.data.data;
+      setSelectedTodo(todo);
+      setTodosAddTask(todo.task || []);
+    } catch (error) {
+      handleError("Failed to load task", error);
+    }
+  };
+
+  // 🔥 ADD TODO TO DB
+  const addTodoToDB = async () => {
+    try {
+      const res = await axios.put(
+        `${baseURL}/task/${selectedTodo._id}`,
+        {
+          text: newTodoText,
+          completed_todo: false,
+          impotent: false,
+        },
+        {
+          headers: { Authorization: `Bearer ${getToken()}` },
+        },
+      );
+
+      setSelectedTodo(res.data.data);
+      setTodosAddTask(res.data.data.task);
+      setNewTodoText("");
+      // const tempTodo = {
+      //   // _id: Date.now(), // temporary id
+      //   text: newTodoText,
+      //   completed_todo: false,
+      //   impotent: false,
+      // };
+
+      // setTodosAddTask((prev) => [...prev, tempTodo]);
+      // setNewTodoText("");
+
+      handleSuccess("Todo added");
+    } catch (err) {
+      handleError("Error adding todo", err);
+    }
+  };
+  const formatDate = (noteDetails) => {
+    // if (!date) return "N/A"; // safety
+
+    return new Date(noteDetails).toLocaleString("en-IN", {
+      day: "2-digit",
+      month: "short",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: true,
+    });
+  };
+  const handleDeletetask = async (id) => {
+    try {
+      await axios.delete(`${baseURL}/task/${id}`, {
+        headers: { Authorization: `Bearer ${getToken()}` },
+      });
+
+      // 🔥 UI update (task list se remove)
+      setTask((prev) => prev.filter((t) => t._id !== id));
+
+      // 🔥 right side reset
+      setSelectedTodo(null);
+
+      handleSuccess("Task deleted");
+    } catch (err) {
+      console.log(err);
+      handleError("Delete failed", err);
+    }
+  };
 
   useEffect(() => {
     setLoggedInUserName(localStorage.getItem("loggedInUserName") || "");
     setLoggedInName(localStorage.getItem("loggedInName") || "");
     fetchNotes();
-    console.log(taskDetails)
   }, []);
 
   return (
@@ -93,9 +159,11 @@ function Task() {
         loggedInUserName={loggedInUserName}
         loggedInName={loggedInName}
       />
+
       <div className="hello">
         <div className="contener-note-body">
           <div className="contener-note-slider">
+            {/* NAV */}
             <div className="slider-button">
               <Link
                 to={"/home"}
@@ -104,6 +172,7 @@ function Task() {
               >
                 <span>Notes</span>
               </Link>
+
               <Link
                 to={"/task"}
                 className={`slider-button-signup link ${activeTab === "tasks" ? "active" : ""}`}
@@ -113,6 +182,7 @@ function Task() {
               </Link>
             </div>
 
+            {/* ADD TASK */}
             <div className="slider-add-notes">
               <button
                 className="slider-add-notes_btm"
@@ -120,10 +190,12 @@ function Task() {
               >
                 Add Task
               </button>
+
               {showPopupTask && (
                 <div className="popup-overlay">
                   <div className="popup-box">
                     <h3 className="popup_heder">Add New Task</h3>
+
                     <input
                       type="text"
                       className="popup_box_data"
@@ -131,17 +203,19 @@ function Task() {
                       value={newTitleTask}
                       onChange={(e) => setNewTitleTask(e.target.value)}
                     />
+
                     <h3 className="popup_heder">Choose Priority Task</h3>
+
                     <select
                       className="popup_box_data"
-                      // defaultValue="medium"
                       value={priority}
                       onChange={(e) => setPriority(e.target.value)}
                     >
                       <option value="medium">medium</option>
-                      <option value="low">low </option>
+                      <option value="low">low</option>
                       <option value="high">high</option>
                     </select>
+
                     <div className="popup-buttons">
                       <button onClick={() => setShowPopupTask(false)}>
                         Cancel
@@ -151,9 +225,11 @@ function Task() {
                   </div>
                 </div>
               )}
-              <div class="line1"></div>
+
+              <div className="line1"></div>
+
               <div className="slider-add-notes_btm1">
-                <p>Open Task </p>
+                <p>Open Task</p>
               </div>
               <div className="slider-add-notes_btm1">
                 <p>compleate Todo</p>
@@ -163,16 +239,14 @@ function Task() {
               </div>
             </div>
 
-            <div className={`slider-notes-list  `}>
+            {/* TASK LIST */}
+            <div className="slider-notes-list">
               <p className="slider-note-main-notes">All Task</p>
-              <div class="line"></div>
+              <div className="line"></div>
 
               <div className="scrolbar_web">
                 {taskLoading ? (
-                  <div>
-                    <p className="loader">Loading notes...</p>
-                    <div className="spinner"></div>
-                  </div>
+                  <p className="loader">Loading...</p>
                 ) : task.length === 0 ? (
                   <p className="slider-note-not">No notes found</p>
                 ) : (
@@ -180,21 +254,16 @@ function Task() {
                     {task.map((todo) => (
                       <li
                         key={todo._id}
-                        className={`slider-note-ls ${(() => {
-                          if (todo.priority == "low") return "priority-low";
-                          if (todo.priority == "medium") return "priority-medium";
-                          if (todo.priority == "high") return "priority-high";
-                           return "";
-                        })()}`}
-
-                        onClick={() => {
-                           handleTaskClick(note._id);
-                          setTaskDetails(todo);
-                        }}
+                        className={`slider-note-ls ${
+                          todo.priority === "low"
+                            ? "priority-low"
+                            : todo.priority === "medium"
+                              ? "priority-medium"
+                              : "priority-high"
+                        }`}
+                        onClick={() => handleTaskClick(todo._id)}
                       >
-                        
                         <span>{todo.title}</span>
-                        <span>{todo.priority}</span>
                         <span>⋮</span>
                       </li>
                     ))}
@@ -204,10 +273,189 @@ function Task() {
             </div>
           </div>
 
+          {/* RIGHT SIDE */}
           <div className="contener-note-content">
-            <div>hello</div>
-            <div class="line"></div>
-            <div></div>
+            <div className="content_task">
+              {selectedTodo ? (
+                <div className="task-content-wrapper">
+                  <div className="show_time_main">
+                    <div className="show_time">
+                      <h2>Title Name: {selectedTodo.title}</h2>
+                      <h2>Priority : {selectedTodo.priority}</h2>
+
+                      <button
+                        className="delete_notes"
+                        onClick={() => setShowPopupDeleteTask(true)}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                    {showPopupDeleteTask && (
+                      <div className="popup-overlay">
+                        <div className="popup-box">
+                          <h3 className="delete_note_text">
+                            Are you sure you want to delete this task?
+                          </h3>
+
+                          <div className="popup-buttons">
+                            <button
+                              onClick={() => setShowPopupDeleteTask(false)}
+                            >
+                              Cancel
+                            </button>
+                            <button
+                              onClick={async () => {
+                                await handleDeletetask(selectedTodo._id);
+                                setShowPopupDeleteTask(false);
+                              }}
+                            >
+                              Delete Notes
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    )}
+                    <div className="show_time">
+                      <p>
+                        Created At:
+                        {formatDate(selectedTodo.createdAt)}
+                      </p>
+
+                      <p>
+                        Updated At:
+                        {formatDate(selectedTodo.updatedAt)}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="line"></div>
+
+                  {/* ADD TODO */}
+                  <div className="todo-input-wrapper">
+                    <input
+                      type="text"
+                      className="todo-input"
+                      value={newTodoText}
+                      onChange={(e) => setNewTodoText(e.target.value)}
+                      placeholder="Write task..."
+                    />
+
+                    <button
+                      className="add-btn todo-add-btn"
+                      onClick={addTodoToDB}
+                    >
+                      + Add Todo
+                    </button>
+                  </div>
+
+                  <div className="line"></div>
+
+                  {/* TODO LIST */}
+                  <div className="todo-list-wrapper">
+                    {todosAddTask.map((item) => (
+                      <div
+                        key={item._id}
+                        className={`todo-item todo-row ${item.impotent ? "important" : ""}`}
+                      >
+                        {/* CHECKBOX */}
+                        <input
+                          type="checkbox"
+                          className="todo-checkbox"
+                          checked={item.completed_todo}
+                          onChange={async () => {
+                            try {
+                              const updated = todosAddTask.map((t) =>
+                                t._id === item._id
+                                  ? { ...t, completed_todo: !t.completed_todo }
+                                  : t,
+                              );
+                              setTodosAddTask(updated);
+                              const res = await axios.put(
+                                `${baseURL}/task/${selectedTodo._id}/${item._id}`,
+                                { completed_todo: !item.completed_todo },
+                                {
+                                  headers: {
+                                    Authorization: `Bearer ${getToken()}`,
+                                  },
+                                },
+                              );
+
+                              setSelectedTodo(res.data.data);
+                              setTodosAddTask(res.data.data.task);
+                            } catch (err) {
+                              console.log(err);
+                            }
+                          }}
+                        />
+
+                        {/* TEXT */}
+                        <input
+                          type="text"
+                          className={`todo-text ${item.completed_todo ? "completed" : ""}`}
+                          value={item.text}
+                          onChange={(e) => {
+                            const updated = [...todosAddTask];
+                            updated.find((t) => t._id === item._id).text =
+                              e.target.value;
+                            setTodosAddTask(updated);
+                          }}
+                          onBlur={async () => {
+                            try {
+                              await axios.put(
+                                `${baseURL}/task/${selectedTodo._id}/${item._id}`,
+                                { text: item.text },
+                                {
+                                  headers: {
+                                    Authorization: `Bearer ${getToken()}`,
+                                  },
+                                },
+                              );
+                            } catch (err) {
+                              console.log(err);
+                            }
+                          }}
+                          placeholder="Write task..."
+                        />
+                        <div className="todo-date">
+                          created time {formatDate(item?.createdAt)}
+                        </div>
+
+                        {item.completed_todo && item.completedAt && (
+                          <div className="todo-date completed-time">
+                            completed time {formatDate(item.completedAt)}
+                          </div>
+                        )}
+                        {/* STAR */}
+                        <button
+                          className="star-btn todo-star-btn"
+                          onClick={async () => {
+                            try {
+                              const res = await axios.put(
+                                `${baseURL}/task/${selectedTodo._id}/${item._id}`,
+                                { impotent: !item.impotent },
+                                {
+                                  headers: {
+                                    Authorization: `Bearer ${getToken()}`,
+                                  },
+                                },
+                              );
+
+                              setSelectedTodo(res.data.data);
+                              setTodosAddTask(res.data.data.task);
+                            } catch (err) {
+                              console.log(err);
+                            }
+                          }}
+                        >
+                          {item.impotent ? "⭐" : "☆"}
+                        </button>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ) : (
+                <p className="no-task-text">Select a task</p>
+              )}
+            </div>
           </div>
         </div>
       </div>
